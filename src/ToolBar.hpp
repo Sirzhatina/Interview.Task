fcdafcbc797b0ab7043594ff708663cc714b0ae8
Sirzhatina <mazurkevichsergey@gmail.com>
2024-07-12 00:23:51 +0300

    added utility buttons to the toolbar

--- src/ToolBar.hpp
+++ src/ToolBar.hpp
@@ -1,8 +1,22 @@
 #pragma once
 
 #include <QToolBar>
+#include <QPushButton>
 
 class ToolBar : public QToolBar {
 public:
-	ToolBar(QWidget* parent): QToolBar{parent} {}
+	ToolBar(QWidget* parent)
+		: QToolBar{parent}
+		, m_triangleButton{ new QPushButton{"Triangle"} }
+		, m_rectangleButton{ new QPushButton{"Rectangle"} }
+		, m_ellipseButton{ new QPushButton{"Ellipse"} } {
+			this->addWidget(m_triangleButton);
+		this->addWidget(m_rectangleButton);
+		this->addWidget(m_ellipseButton);
+	}
+
+private:
+	QPushButton* m_triangleButton;
+	QPushButton* m_rectangleButton;
+	QPushButton* m_ellipseButton;
 };
