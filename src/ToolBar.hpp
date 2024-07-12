d2bc047857ac3c1c928f19060b4f2d5b3c05ea04
Sirzhatina <mazurkevichsergey@gmail.com>
2024-07-12 10:04:15 +0300

    made tool bar immovable

--- src/ToolBar.hpp
+++ src/ToolBar.hpp
@@ -2,6 +2,7 @@
 
 #include <QToolBar>
 #include <QPushButton>
+#include <QHBoxLayout>
 
 class ToolBar : public QToolBar {
 public:
@@ -10,7 +11,10 @@ public:
 		, m_triangleButton{ new QPushButton{"Triangle"} }
 		, m_rectangleButton{ new QPushButton{"Rectangle"} }
 		, m_ellipseButton{ new QPushButton{"Ellipse"} } {
-			this->addWidget(m_triangleButton);
+
+		this->setMovable(false);
+
+		this->addWidget(m_triangleButton);
 		this->addWidget(m_rectangleButton);
 		this->addWidget(m_ellipseButton);
 	}
