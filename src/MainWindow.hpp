16c6c31aeea29a72d95af3474b761a4ffa1f5c2a
Sirzhatina <mazurkevichsergey@gmail.com>
2024-07-12 10:37:50 +0300

    set default and minimum window size

--- src/MainWindow.hpp
+++ src/MainWindow.hpp
@@ -12,9 +12,13 @@ public:
     MainWindow() : m_toolBar{ new ToolBar{this} }, m_drawingArea{ new DrawingArea{this} } {
         this->addToolBar(m_toolBar);
         this->setCentralWidget(m_drawingArea);
+
+        this->setMinimumSize(DEFAULT_WINDOW_SIZE);
     }
 
 private:
     ToolBar* m_toolBar;
     DrawingArea* m_drawingArea;
+
+    static constexpr QSize DEFAULT_WINDOW_SIZE{ 640, 480 };
 };
