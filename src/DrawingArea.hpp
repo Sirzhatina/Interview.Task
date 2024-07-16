69f49d5c86bad7ff799cc5f28c4edb2abe1cc846
Sirzhatina <mazurkevichsergey@gmail.com>
2024-07-16 08:44:59 +0300

    added method for mouse press event

--- src/DrawingArea.hpp
+++ src/DrawingArea.hpp
@@ -1,11 +1,17 @@
 #pragma once
 
 #include <QWidget>
+#include <QMouseEvent>
 #include "MainWindow.hpp"
 
 class DrawingArea : public QWidget {
 public:
 	DrawingArea(MainWindow* parent) : QWidget{parent} {
+
+	}
+
+protected:
+	void mousePressEvent(QMouseEvent* e) override {
 
 	}
 };
