9727db0ec40e2c45f13be2606e10aa37398a6dfa
Sirzhatina <mazurkevichsergey@gmail.com>
2024-07-21 22:42:19 +0300

    made the AbstractShape::draw return std::expected

--- src/Shapes/AbstractShape.cpp
+++ src/Shapes/AbstractShape.cpp
@@ -1,9 +1,13 @@
 #include "AbstractShape.hpp"
 
 
-void AbstractShape::draw(QPainter* p) {
+AbstractShape::DrawResult AbstractShape::draw(QPainter* p) {
 	if (p == nullptr) {
-
+		return std::unexpected{ PainterError::NULL_PASSED };
 	}
-	auto pd = p->device();
+	if (p->device() == nullptr) {
+		return std::unexpected{ PainterError::NO_DEVICE_BOUND };
+	}
+
+	return DrawResult{};
 }
--- src/Shapes/AbstractShape.hpp
+++ src/Shapes/AbstractShape.hpp
@@ -1,11 +1,19 @@
 #pragma once
 
 #include <QPainter>
+#include <expected>
 
 
 class AbstractShape {
 public:
-	virtual void draw(QPainter* p) = 0;
 
+	enum class PainterError {
+		NULL_PASSED,
+		NO_DEVICE_BOUND
+	};
 
+	using DrawResult = std::expected<void, PainterError>;
+
+public:
+	virtual DrawResult draw(QPainter* p) = 0;
 };
